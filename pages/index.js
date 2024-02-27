569e334203b55032e8217e0c1dcd141029351f09
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-02-27 18:42:53 -0300

    fix: corrige texto da primeira frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
     return <>
-        <h1>Olá esse é um deploy de teste para o curso.de</h1>
+        <h1>Olá esse é um deploy de teste para o curso.dev</h1>
         <p>
             These aren't the droids you're looking for
         </p>
