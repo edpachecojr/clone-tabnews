e0123f03677ebf780605f24c498af43bafdf5125
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-02-27 18:24:23 -0300

    feat: desafio 1 curso.dev

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return <>
+        <h1>Olá esse é um deploy de teste para o curso.de</h1>
+        <p>
+            These aren't the droids you're looking for
+        </p>
+    </>
+
+}
