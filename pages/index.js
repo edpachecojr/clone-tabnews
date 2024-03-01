89a48d9bf688919d34ff0e901c79de9dbe2e3d20
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-03-01 15:50:58 -0300

    adiciona arquivo .editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 export default function Home() {
-    return <>
-        <h1>Olá esse é um deploy de teste para o curso.dev</h1>
-        <p>
-            These aren't the droids you're looking for
-        </p>
-    </>
+  return <>
+    <h1>Olá esse é um deploy de teste para o curso.dev</h1>
+    <p>
+      These aren't the droids you're looking for
+    </p>
+  </>
 
 }
