7e7947a802dceb164b4105d08aba9b674929c46f
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-03-11 22:37:28 -0300

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -10,10 +10,15 @@ async function query(queryObject) {
   });
 
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+  try {
+    const result = await client.query(queryObject);
 
-  return result;
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
