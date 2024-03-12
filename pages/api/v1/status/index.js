493501ce14a8616212edea72ce4ddd35dfb5781c
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-03-11 22:38:05 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,24 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW MAX_CONNECTIONS;");
+  const databaseName = process.env.POSTGRES_DB;
+
+  const activeConnections = await database.query({
+    text: "SELECT COUNT(1)::int as activeConnections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: activeConnections.rows[0].activeconnections,
+      },
+    },
+  });
 }
